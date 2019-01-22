a86b192689eb2bfa22c3b25c5df8ca4c74376eca
LiuQianyuan <1073133884@qq.com>
2019-01-22 21:45:52 +0800

    Change num_falg to num_flag.

--- ttest_001.py
+++ ttest_001.py
@@ -1,13 +1,13 @@
 #coding=utf-8
 # 本题为考试单行多行输入输出规范示例，无需提交，不计分。
 import sys
-for lines in sys.stdin:
-    a, b, c = lines.split()
+for line in sys.stdin:
+    a, b, c = line.split()
     # print(int(a[0]) + int(a[1]))
     # print(a, b, c)
-    a,b,c = list(map(int, lines.split()))
+    a,b,c = list(map(int, line.split()))
     num = 0
-    a_flag = b_flag = c_flag = num_falg = 0
+    a_flag = b_flag = c_flag = num_flag = 0
     for i_a in range(a+1):
         for i_b in range(b+1):
             for i_c in range(c+1):
@@ -19,8 +19,8 @@ for lines in sys.stdin:
                 if 1<=i_c<=c:
                     c_flag = 1
                 if (i_a + i_b < i_c) or (i_a + i_c < i_b) or (i_c + i_b < i_a):
-                    num_falg = 1
-                if a_flag and b_flag and c_flag and num_falg :
+                    num_flag = 1
+                if a_flag and b_flag and c_flag and num_flag :
                     num = num + 1
-                    a_flag = b_flag = c_flag = num_falg = 0
+                    a_flag = b_flag = c_flag = num_flag = 0
     print(num%1000000007)
